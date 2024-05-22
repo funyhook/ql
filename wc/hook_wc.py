fe3e8fccc350b2f6cc3238b53fce5c7fb0677858
lsy <shaoyang_lian@uyoung.com.cn>
2024-05-22 11:49:37 +0800

    yuer 葫芦娃 哈喽

--- wc/hook_wc.py
+++ wc/hook_wc.py
@@ -1,22 +1,14 @@
-"""
-cron: 0 9 * * *
-new Env('app_望潮');
-* 反馈群：https://t.me/vhook_wool
-"""
-import sys
-from  utils import notify,common
-print(notify.push_config)
-print(common.PYTHON_VERSION)
-if __name__ == '__main__':
 
+import sys
+if __name__ == '__main__':
     version_info = sys.version_info
     print("Python 版本：", sys.version[:4], version_info.minor)
-    path = f"../wc/{version_info.minor}.py"
     try:
-        with open(path, "r", encoding="utf-8") as f:
+        with open("../wc/"+str(version_info.minor)+".py", "r", encoding="utf-8") as f:
             data = f.read()
             if data:
                 exec(data)
     except Exception as e:
         print(e.with_traceback())
         print("Python 版本：", sys.version)
+        
